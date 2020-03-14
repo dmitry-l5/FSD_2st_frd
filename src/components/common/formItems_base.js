65684fe3ecfb41466d785e36852e1bc2700a953a
dmitry-l5 <60506213+dmitry-l5@users.noreply.github.com>
2020-03-15 00:19:53 +0900

    добавил элемент chekbox

---
+++ src/components/common/formItems_base.js
@@ -0,0 +1,17 @@
+function say() {
+    console.log("femcmdcakmc[owkejf[oierjg[oribofdskmcoKASMSD[OM[FOMDSFO[GSTRGOSTRORTOO");
+}
+function setFormItem(id) {
+    if (window.formItems == void 0) {
+         window.formItems = [];
+    }
+    let tmp = {
+        id: id
+    };
+    formItems.push(tmp);
+    console.log(formItems);
+}
+
+function infoFormItems() {
+    console.log(window.formItems);
+}
